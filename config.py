b25b9774d571fe1d71f972b6942848f70e3f745f
appchecker <62602594+clight917@users.noreply.github.com>
2025-02-17 13:58:09 +0800

    config.py

--- config.py
+++ config.py
@@ -23,20 +23,20 @@ curl_str = os.getenv('WXREAD_CURL_BASH')
 
 # headers、cookies是一个省略模版，本地或者docker部署时对应替换
 cookies = {
-    'RK': 'oxEY1bTnXf',
-    'ptcz': '53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b',
-    'pac_uid': '0_e63870bcecc18',
-    'iip': '0',
-    '_qimei_uuid42': '183070d3135100ee797b08bc922054dc3062834291',
-    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FeEOpSbFh2Mb1bUxMW9Y3FRPfXwWvOLaNlsjWIkcKeeNg6vlVS5kOVuhNKGQ1M8zaggLqMPmpE5qIUdqEXlQgYg%2F132',
-    'wr_gender': '0',
+    'RK': 'oxEY1bTnXf'，
+    'ptcz': '53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b'，
+    'pac_uid': '0_e63870bcecc18'，
+    'iip': '0'，
+    '_qimei_uuid42': '183070d3135100ee797b08bc922054dc3062834291'，
+    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FeEOpSbFh2Mb1bUxMW9Y3FRPfXwWvOLaNlsjWIkcKeeNg6vlVS5kOVuhNKGQ1M8zaggLqMPmpE5qIUdqEXlQgYg%2F132'，
+    'wr_gender': '0'，
 }
 
 headers = {
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5',
-    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
+    'accept': 'application/json, text/plain, */*'，
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5'，
+    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862'，
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'，
 }
 
 
@@ -44,20 +44,21 @@ headers = {
 建议保留区域|默认读三体，其它书籍自行测试时间是否增加
 """
 data = {
-    "appId": "wb182564874663h152492176",
-    "b": "ce032b305a9bc1ce0b0dd2a",
-    "c": "7cb321502467cbbc409e62d",
-    "ci": 70,
-    "co": 0,
-    "sm": "[插图]第三部广播纪元7年，程心艾AA说",
-    "pr": 74,
-    "rt": 30,
-    "ts": 1727660516749,
-    "rn": 31,
-    "sg": "991118cc229871a5442993ecb08b5d2844d7f001dbad9a9bc7b2ecf73dc8db7e",
-    "ct": 1727660516,
-    "ps": "b1d32a307a4c3259g016b67",
-    "pc": "080327b07a4c3259g018787",
+   'appId': 'wb115321887466h1684985596',
+    'b': 'f7932970813ab6e84g019b96',
+    'c': '02e32f0021b02e74f10ece8',
+    'ci': 3,
+    'co': 352,
+    'sm': '[插图]事件之章野野口修的手记',
+    'pr': 0,
+    'rt': 7,
+    'ts': 1739765482348,
+    'rn': 94,
+    'sg': '54d4f2bc759414fe480eb4095e85243bd8ee18286da934ac981f2d1415c0537d',
+    'ct': 1739765482,
+    'ps': 'cbe326307a5eabe2g01930a',
+    'pc': '535323007a5eabe3g013a3e',
+    's': 'c44fb2f0',
 }
 
 
@@ -69,9 +70,9 @@ def convert(curl_command):
 
     # 提取 cookies
     cookies = {}
-    cookie_string = headers.pop('cookie', '')
+    cookie_string = headers.pop('cookie'， '')
     for cookie in cookie_string.split('; '):
-        key, value = cookie.split('=', 1)
+        key, value = cookie.split('='， 1)
         cookies[key] = value
 
     return headers, cookies
